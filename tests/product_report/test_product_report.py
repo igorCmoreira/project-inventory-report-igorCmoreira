9b25c6a10e96f22434e991c5d1eb7dfd9651b99c
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-07 19:54:18 -0300

    Requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        2,
+        "farinha",
+        "farini",
+        "01-05-2021",
+        "02-06-2023",
+        123457,
+        "Ao abrigo de luz",
+    )
+    assert product.__repr__() == (
+        "O produto farinha"
+        " fabricado em 01-05-2021"
+        " por farini com validade"
+        " até 02-06-2023"
+        " precisa ser armazenado Ao abrigo de luz."
+    )
