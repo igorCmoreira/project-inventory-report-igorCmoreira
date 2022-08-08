fba3f3cec2e3a66daf5985878796edc57df5cf13
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-08 01:48:45 -0300

    teste colored

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,19 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+mock = [
+    {
+        "nome_da_empresa": "farinha",
+        "data_de_fabricação": "2022-09-03",
+        "data_de_validade": "2025-12-12",
+    }
+]
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    relatorio = ColoredReport(SimpleReport).generate(mock)
+
+    assert "\033[36m2022-09-03\033[0m" in relatorio
+    assert "\033[36m2025-12-12\033[0m" in relatorio
+    assert "\033[31mfarinha\033[0m" in relatorio
